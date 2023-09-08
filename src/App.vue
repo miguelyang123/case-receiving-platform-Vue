bf96425ea1632583fbce440e5f1b9ca82d1cea0d
hibiki4945 <wc4910720@gmail.com>
2023-09-08 16:49:37 +0800

    01

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script>
 import HomeView from "./views/HomeView.vue";
+import PersonalInfo from "./views/PersonalInfo.vue";
 export default {
   components: {
     HomeView,
+    PersonalInfo,
   },
 };
 </script>
 
 <template>
   <HomeView />
+  <PersonalInfo />
 </template>
 
 <style scoped></style>
---
+++ src/views/PersonalInfo.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <h1>Speed接案網</h1>
+  <p>Personal Info</p>
+</template>
+
+<style lang="scss" scoped></style>
