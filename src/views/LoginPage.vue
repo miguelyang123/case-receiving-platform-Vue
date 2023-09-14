a851ea53828b666c525d310c44feec9e981ddb02
syaun <clou40308@gmail.com>
2023-09-14 15:07:29 +0800

    UPDATE:增加密碼 明碼暗碼功能

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,8 +1,28 @@
 <script >
-
+export default {
+    data() {
+        return {
+            pwdFlag: false, //密碼明碼
+            password: "",
+        }
+    },
+    methods: {
+        pwdflagTrue() {
+            this.pwdFlag = true;
+        },
+        pwdflagFalse() {
+            this.pwdFlag = false;
+        },
+    },
+}
 </script>
 
 <template>
+    <!-- font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <div class="flex justify-center mt-[50px]  h-screen w-screen">
         <div class="border-2 border-black rounded-[30px] w-[500px] h-[500px]">
             <div class="flex  h-[100px] justify-center items-center ">
@@ -10,32 +30,34 @@
             </div>
             <div class="h-[220px] ">
                 <div class="relative">
-                    <input type="text" value="輸入Email"
+                    <input type="text" placeholder="輸入Email"
                         class="border-2 border-black ml-[50px] mt-[40px] w-[400px] h-[55px] text-[24px] rounded-lg pl-[60px]">
                     <div
                         class="absolute w-[35px] h-[35px] bg-[url('../../public/Account_icon.png')] bg-cover bottom-[9px] left-[60px]">
                     </div>
                 </div>
                 <div class="relative">
-                    <input type="text" value="輸入密碼"
-                        class="border-2 border-black ml-[50px] mt-[50px] w-[400px] h-[55px] text-[24px] rounded-lg pl-[60px]">
+                    <input placeholder="輸入密碼" :type="pwdFlag ? 'text' : 'password'" v-model="password"
+                        class="border-2 border-black ml-[50px] mt-[50px] w-[400px] h-[55px] text-[24px] rounded-lg px-[60px]">
                     <div
                         class="absolute w-[35px] h-[35px] bg-[url('../../Password_icon.png')] bg-cover bottom-[9px] left-[60px]">
                     </div>
-
+                    <i class="absolute bottom-[25px] left-[400px] fa-regular fa-eye fa-2xl" @click="pwdflagTrue"
+                        v-if="this.pwdFlag === false"></i>
+                    <i class="absolute bottom-[25px] left-[400px] fa-regular fa-eye-slash fa-2xl" @click="pwdflagFalse"
+                        v-if="this.pwdFlag === true"></i>
                 </div>
             </div>
-            <div class="h-[50px]  flex  justify-between px-[60px]" >
+            <div class="h-[50px]  flex  justify-between px-[60px]">
                 <button type="button" class="text-[22px] font-bold hover:scale-105 active:scale-95 ">會員註冊</button>
                 <button type="button" class="text-[22px] font-bold hover:scale-105 active:scale-95 ">忘記密碼</button>
             </div>
             <div class="flex  h-[120px] justify-center items-center ">
-                <button type="button" class="w-[220px] h-[60px]  text-[26px] rounded-lg bg-[#2B4BF0] text-white hover:scale-105 active:scale-95">登入</button>
+                <button type="button"
+                    class="w-[220px] h-[60px]  text-[26px] rounded-lg bg-[#2B4BF0] text-white hover:scale-105 active:scale-95">登入</button>
             </div>
         </div>
     </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,12 +1,36 @@
 <script>
 export default {
-  setup() {
-    return {};
+  data() {
+    return {
+      pwdFlag: false, //密碼明碼
+      repeatpwdFlag: false, //repeat 密碼明碼
+      password: "",
+      repeatPassword: "",
+    }
   },
-};
+  methods: {
+    pwdflagTrue() {
+      this.pwdFlag = true;
+    },
+    pwdflagFalse() {
+      this.pwdFlag = false;
+    },
+    repeatpwdflagTrue() {
+      this.repeatpwdFlag = true;
+    },
+    repeatpwdflagFalse() {
+      this.repeatpwdFlag = false;
+    },
+  },
+}
 </script>
 
 <template>
+  <!-- font-awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+
   <div class="flex justify-center mt-[50px]  h-screen w-screen">
     <div class="border-2 border-black rounded-[30px] w-[1150px] h-[600px]">
       <div class="flex  h-[100px] justify-center items-center">
@@ -15,51 +39,57 @@ export default {
       <div class="flex">
         <div class="w-6/12  h-[350px]">
           <div class="relative">
-            <input type="text" value="輸入您的真實姓名"
+            <input type="text" placeholder="輸入您的真實姓名"
               class="border-2 border-black ml-[50px] mt-[80px] w-[400px] h-[55px] text-[24px] rounded-lg pl-[60px]">
             <div
               class="absolute w-[35px] h-[35px] bg-[url('../../public/icon-park-twotone_edit-name.png')] bg-cover bottom-[9px] left-[60px]">
             </div>
           </div>
           <div class="relative">
-            <input type="text" value="輸入您的手機門號"
+            <input type="text" placeholder="輸入您的手機門號"
               class="border-2 border-black ml-[50px] mt-[50px] w-[400px] h-[55px] text-[24px] rounded-lg pl-[60px]">
-              <div
+            <div
               class="absolute w-[35px] h-[35px] bg-[url('../../public/ph_phone-duotone.png')] bg-cover bottom-[9px] left-[60px]">
             </div>
           </div>
         </div>
         <div class="w-6/12  h-[350px]">
           <div class="relative">
-            <input type="text" value="輸入Email"
+            <input type="text" placeholder="輸入Email"
               class="border-2 border-black ml-[50px] mt-[40px] w-[400px] h-[55px] text-[24px] rounded-lg pl-[60px]">
             <div
               class="absolute w-[35px] h-[35px] bg-[url('../../public/Account_icon.png')] bg-cover bottom-[9px] left-[60px]">
             </div>
           </div>
           <div class="relative">
-            <input type="text" value="輸入密碼"
-              class="border-2 border-black ml-[50px] mt-[50px] w-[400px] h-[55px] text-[24px] rounded-lg pl-[60px]">
-              <div
-              class="absolute w-[35px] h-[35px] bg-[url('../../Password_icon.png')] bg-cover bottom-[9px] left-[60px]">
+            <input  placeholder="輸入密碼"  :type="pwdFlag ? 'text' : 'password'"  v-model="password" 
+              class="border-2 border-black ml-[50px] mt-[50px] w-[400px] h-[55px] text-[24px] rounded-lg px-[60px]" >
+            <div class="absolute w-[35px] h-[35px] bg-[url('../../Password_icon.png')] bg-cover bottom-[9px] left-[60px]">
             </div>
- 
+            <i class="absolute bottom-[25px] left-[400px] fa-regular fa-eye fa-2xl" @click="pwdflagTrue"
+              v-if="this.pwdFlag === false"></i>
+            <i class="absolute bottom-[25px] left-[400px] fa-regular fa-eye-slash fa-2xl" @click="pwdflagFalse"
+              v-if="this.pwdFlag === true"></i>
           </div>
-          <div class="relative">
-            <input type="text" value="確認密碼"
-              class="border-2 border-black ml-[50px] mt-[50px] w-[400px] h-[55px] text-[24px] rounded-lg pl-[60px]">
-              <div
-              class="absolute w-[35px] h-[35px] bg-[url('../../Password_icon.png')] bg-cover bottom-[9px] left-[60px]">
+          <div class="relative ">
+            <input placeholder="確認密碼" :type="repeatpwdFlag ? 'text' : 'password'"  v-model="repeatPassword"
+              class="border-2 border-black ml-[50px] mt-[50px] w-[400px] h-[55px] text-[24px] rounded-lg px-[60px] ">
+            <div class="absolute w-[35px] h-[35px] bg-[url('../../Password_icon.png')] bg-cover bottom-[9px] left-[60px]">
             </div>
- 
+            <i class="absolute bottom-[25px] left-[400px] fa-regular fa-eye fa-2xl" @click="repeatpwdflagTrue"
+              v-if="this.repeatpwdFlag === false"></i>
+            <i class="absolute bottom-[25px] left-[400px] fa-regular fa-eye-slash fa-2xl" @click="repeatpwdflagFalse"
+              v-if="this.repeatpwdFlag === true"></i>
           </div>
         </div>
       </div>
       <div class=" h-[100px] flex justify-center items-center">
-         <button type="button" class="w-[250px] h-[60px]  text-[26px] rounded-lg bg-[#2B4BF0] text-white hover:scale-105 active:scale-95">註冊</button>
+        <button type="button"
+          class="w-[250px] h-[60px]  text-[26px] rounded-lg bg-[#2B4BF0] text-white hover:scale-105 active:scale-95">註冊</button>
       </div>
       <div class=" h-[50px] flex justify-center ">
-         <button type="button" class="w-[180px] h-[35px]  text-[22px] rounded-lg  font-bold hover:scale-105 active:scale-95">已有帳號?登入</button>
+        <button type="button"
+          class="w-[180px] h-[35px]  text-[22px] rounded-lg  font-bold hover:scale-105 active:scale-95">已有帳號?登入</button>
       </div>
     </div>
   </div>
