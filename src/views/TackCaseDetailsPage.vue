6c13acd2ecd3c2b9a2092c70180a6a6b0a60249b
roottnt <cherhorn538@gmail.com>
2023-09-14 18:32:44 +0800

    1. 更新接案子頁面 2. 更新案子詳細資料頁面

--- src/views/TackCaseDetailsPage.vue
+++ src/views/TackCaseDetailsPage.vue
@@ -1,32 +1,80 @@
 <script>
+import { RouterLink, RouterView } from 'vue-router';
 export default {
+    comments: {
+        RouterLink
+    },
     props:["thisList"],
     data() {
         return {
-            
+            arrList:JSON.parse(this.thisList)
         }
     },
+    computed:{
+        newArrList(){
+            let arr = {};
+
+            Object.entries(this.arrList).forEach((item,index) =>{
+                if(['price','location','caseDate'].includes(item[0])){
+                    arr[item[0]]=item[1];
+                }
+            })
+
+            return arr;
+        }
+    }
 }
 </script>
 <template>
     <div class="w-[70%] mx-auto my-12">
-       <h1 class="text-2xl font-bold px-6 py-3 border-b-2 border-b-black">照片編輯</h1>
-       <div class="flex text-2xl"> 
-            <div class="mr-12">
-                <div v-for="(item,index) in ['預算金額','執行地點','案子到期日']" class=" my-6">
-                    <p class="font-bold">{{ item }}</p>
+        <div class="border border-[#aaa9a9] px-12 py-6 rounded-xl">
+            <h1 class="text-2xl font-bold px-6 py-3 border-b-2 border-b-black">{{ arrList.title }}</h1>
+            <div class="flex text-2xl"> 
+                <div class="mr-12">
+                    <div v-for="(item,index) in ['預算金額','執行地點','案子到期日']" class=" my-6">
+                        <p class="font-bold">{{ item }}</p>
+                    </div>
+                </div>
+                <div>
+                    <div v-for="(item,index,arr) in newArrList" class="flex items-center my-6">
+                        <img width="15" class="mr-3" v-if=" arr !== 0" :src="'/src/assets/img/TackCaseImg/' + ( arr===1 ? 'location.png' : 'date.png') " alt="">
+                        <p>{{ item }}</p>
+                    </div>
                 </div>
             </div>
-            <div>
-                <div v-for="(item,index,arr) in {'預算金額':50000,'執行地點':'台南','案子到期日':'2023/09/30'}" class="flex items-center my-6">
-                    <img width="15" class="mr-3" v-if=" arr !== 0" :src="'/src/assets/img/TackCaseImg/' + ( arr===1 ? 'location.png' : 'date.png') " alt="">
-                    <p>{{ item }}</p>
-                </div>
+        </div>
+        <div class="border border-[#aaa9a9] px-12 py-6 rounded-xl my-6">
+            <h1 class="text-2xl font-bold px-6 py-3 border-b-2 border-b-black">服務內容</h1>
+            <div class=" whitespace-pre-wrap mt-6 mb-12">
+                {{ arrList.content }}
+            </div>
+            <div class=" text-center py-3">
+                <RouterLink to="/"
+                    id="tackCase" class=" tackCase">
+                    我要接案
+                </RouterLink>
             </div>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-    
+    .tackCase{
+        color: white;
+        border-radius: 0.5rem;
+        background: #FF6E6E;
+        padding: 0.75rem 1.5rem;
+        font-size: 1.5rem; line-height: 2rem;
+        font-weight: 700;
+
+        
+    }
+    #tackCase{
+        &:hover{
+            scale: 1.05;
+        }
+        &:active{
+            scale: 0.95;
+        }
+    }
 </style>
--- src/views/TackCasePage.vue
+++ src/views/TackCasePage.vue
@@ -1,81 +1,97 @@
 <script>
-import { RouterLink, RouterView} from 'vue-router';
+import { RouterLink, RouterView } from 'vue-router';
 export default {
-    comments:{
+    comments: {
         RouterLink
     },
 
     data() {
         return {
 
-            thisType:["red",""],
+            thisType: ["text-red-600", ""],
 
-            mapShow:false,
-            map:["地區不限",
-                "臺北","新北","基隆","桃園","新竹","宜蘭",
-                "苗栗","臺中","彰化","南投","雲林",
-                "嘉義","臺南","高雄","屏東","澎湖",
-                "花蓮","臺東",
-                "金門","連江"
+            mapShow: false,
+            map: ["地區不限",
+                "臺北", "新北", "基隆", "桃園", "新竹", "宜蘭",
+                "苗栗", "臺中", "彰化", "南投", "雲林",
+                "嘉義", "臺南", "高雄", "屏東", "澎湖",
+                "花蓮", "臺東",
+                "金門", "連江"
             ],
-            thisMap:"地區不限",
+            thisMap: "地區不限",
 
-            priceShow:false,
-            price:["案件金額不限","5千以下","5001 ~ 1萬","1萬 ~ 5萬"],
-            thisPrice:"案件金額",
+            priceShow: false,
+            price: ["案件金額不限", "5千以下", "5001 ~ 1萬", "1萬 ~ 5萬"],
+            thisPrice: "案件金額",
 
-            thisKeyWord:"",
+            thisKeyWord: "",  // 關鍵字
 
-            page:1,      // 當前頁數
-            allPage:15,  // 總頁數
-            pageNum:5    // 分頁數量
+            arrList: [{
+                'title': '照片編輯', 'price': 50000, 'location': '台南', 'caseDate': '2023/09/30', 'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit.             Totam tempora aut soluta dolor nisi recusandae   \n   \n  dolorum aperiam, repellat doloremque placeat, sunt dignissimos quasi quos pariatur impedit itaque. Praesentium quod cumque ratione nobis iusto aliquam repudiandae dolor sapiente enim doloremque minima ad officiis placeat aspernatur illum reiciendis modi, itaque quo? Nemo?'
+            }, {
+                'title': '影片編輯', 'price': 30000, 'location': '新北', 'caseDate': '2023/10/20', 'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit.             Totam tempora aut soluta dolor nisi recusandae   \n   \n   doloremque placeat, sunt dignissimos quasi quos pariatur impedit itaque. Praesentium quod cumque ratione nobis iusto aliquam repudiandae dolor sapiente enim doloremque minima ad officiis placeat aspernatur illum reiciendis modi, itaque quo? Nemo?'
+            }],
+
+            page: 1,      // 當前頁數
+            allPage: 15,  // 總頁數
+            pageNum: 5    // 分頁數量
         }
     },
-    methods:{
-        changeMap(index1){
-            this.thisMap=index1;
-            this.mapShow=false;
+    methods: {
+
+        // 顯示點選哪個類別
+        thisType1(index) {
+            if (index === 0) {
+                if (this.thisType[1] === '線上') { this.thisType[1] = '' } else { this.thisType[1] = '線上' }
+            } else {
+                if (this.thisType[1] === '線下') { this.thisType[1] = '' } else { this.thisType[1] = '線下' }
+            }
         },
-        changePrice(index1){
-            this.thisPrice=index1;
-            this.priceShow=false;
+
+        changeMap(index1) {
+            this.thisMap = index1;
+            this.mapShow = false;
+        },
+        changePrice(index1) {
+            this.thisPrice = index1;
+            this.priceShow = false;
         },
 
         // 上一頁
-        pageBlank(){
-            if(this.page!==1){
+        pageBlank() {
+            if (this.page !== 1) {
                 this.page -= 1;
             }
         },
         //下一頁
-        pageNext(){
-            if(this.page!==this.allPage){
+        pageNext() {
+            if (this.page !== this.allPage) {
                 this.page += 1;
             }
         },
-        
+
     },
-    computed:{
-        pageNumCheck(){
+    computed: {
+        pageNumCheck() {
 
-            let fistPage,endPage;  // 分頁開始, 分頁結束
-            let arr=[]; 
+            let fistPage, endPage;  // 分頁開始, 分頁結束
+            let arr = [];
 
-            if(this.allPage <= this.pageNum){  // 當總頁數小於設定頁數
+            if (this.allPage <= this.pageNum) {  // 當總頁數小於設定頁數
                 fistPage = 1;
                 endPage = this.allPage;
-            }else if(this.page <= Math.floor(this.pageNum/2)+1 ){   // 當前頁數 <= 設定頁數/2 +1
+            } else if (this.page <= Math.floor(this.pageNum / 2) + 1) {   // 當前頁數 <= 設定頁數/2 +1
                 fistPage = 1;
                 endPage = this.pageNum;
-            }else if(this.page + Math.floor(this.pageNum/2) >= this.allPage){  // 當前頁數 + 設定頁數/2 >= 總頁數
+            } else if (this.page + Math.floor(this.pageNum / 2) >= this.allPage) {  // 當前頁數 + 設定頁數/2 >= 總頁數
                 endPage = this.allPage;
-                fistPage = this.allPage - this.pageNum+1;
-            }else{ 
-                fistPage = this.page - Math.floor(this.pageNum/2);
-                endPage = this.page + Math.floor(this.pageNum/2);
+                fistPage = this.allPage - this.pageNum + 1;
+            } else {
+                fistPage = this.page - Math.floor(this.pageNum / 2);
+                endPage = this.page + Math.floor(this.pageNum / 2);
             }
 
-            for(let i = fistPage; i <= endPage ; i++){
+            for (let i = fistPage; i <= endPage; i++) {
                 arr.push(i);
             }
 
@@ -88,38 +104,46 @@ export default {
     <div class="w-[85%] mx-auto py-6 mb-[10rem]">
         <h1 class="text-3xl font-bold">找案件</h1>
         <div class="flex my-6">
-            <button class="findCaseType"  @click="thisType[1]='線上'">
-                <iconify-icon icon="fluent-mdl2:join-online-meeting" :class="{ 'hover:text-red-600':true,'text-red-600' :thisType[1]==='線上' }" width="65"></iconify-icon>
-                <p :class="{ 'hover:text-red-600':true,'text-red-600' :thisType[1]==='線上' }">遠端</p>
+            <button class="findCaseType" @click="thisType1(0)">
+                <iconify-icon icon="fluent-mdl2:join-online-meeting" :class="{ 'text-red-600': thisType[1] === '線上' }"
+                    width="65"></iconify-icon>
+                <p :class="{ 'text-red-600': thisType[1] === '線上' }">遠端</p>
             </button>
-            <button class="findCaseType" @click="thisType[1]='線下'">
-                <iconify-icon icon="healthicons:domestic-worker" :class="{ 'hover:text-red-600':true,'text-red-600' :thisType[1]==='線下' }" width="65"></iconify-icon>
-                <p :class="{ 'hover:text-red-600':true,'text-red-600' :thisType[1]==='線下' }">線場</p>
+            <button class="findCaseType" @click="thisType1(1)">
+                <iconify-icon icon="healthicons:domestic-worker" :class="{ 'text-red-600': thisType[1] === '線下' }"
+                    width="65"></iconify-icon>
+                <p :class="{ 'text-red-600': thisType[1] === '線下' }">線場</p>
             </button>
         </div>
         <div class="flex">
-            <div class=" relative" @mouseover="mapShow=true" @mouseleave="mapShow=false">
+            <div class=" relative" @mouseover="mapShow = true" @mouseleave="mapShow = false">
                 <div class=" filter">
                     <p>{{ thisMap }}</p>
                     <p class="text-lg ml-3">V</p>
                 </div>
                 <div v-if="mapShow" class="mapAndPriceSelect shadow-xl">
-                    <div  v-for="(item,index) in map" class=" text-[1rem] font-bold py-3 pl-6 pr-10 cursor-pointer hover:bg-blue-300 hover:text-white" @click="changeMap(item)">
-                        <input type="radio" :id="'map'+index" name="thismap" class="mr-3 cursor-pointer" :value="item" :checked=" thisMap === item">
-                        <label :for="'map'+index" class="cursor-pointer">{{ item }}</label>
+                    <div v-for="(item, index) in map"
+                        class=" text-[1rem] font-bold py-3 pl-6 pr-10 cursor-pointer hover:bg-blue-300 hover:text-white"
+                        @click="changeMap(item)">
+                        <input type="radio" :id="'map' + index" name="thismap" class="mr-3 cursor-pointer" :value="item"
+                            :checked="thisMap === item">
+                        <label :for="'map' + index" class="cursor-pointer">{{ item }}</label>
                     </div>
                 </div>
             </div>
 
-            <div class=" relative" @mouseover="priceShow=true" @mouseleave="priceShow=false">
+            <div class=" relative" @mouseover="priceShow = true" @mouseleave="priceShow = false">
                 <div class=" filter" style="width: 210px;">
                     <p>{{ thisPrice }}</p>
                     <p class="text-lg ml-3">V</p>
                 </div>
                 <div v-if="priceShow" class="mapAndPriceSelect shadow-xl">
-                    <div  v-for="(item,index) in price" class="text-[1rem] font-bold py-3 pl-6 pr-10 cursor-pointer hover:bg-blue-300 hover:text-white" @click="changePrice(item)">
-                        <input type="radio" :id="'price'+index" name="thisPrice" class="mr-3 cursor-pointer" :value="item" :checked=" thisPrice === item">
-                        <label :for="'price'+index" class="cursor-pointer">{{ item }}</label>
+                    <div v-for="(item, index) in price"
+                        class="text-[1rem] font-bold py-3 pl-6 pr-10 cursor-pointer hover:bg-blue-300 hover:text-white"
+                        @click="changePrice(item)">
+                        <input type="radio" :id="'price' + index" name="thisPrice" class="mr-3 cursor-pointer" :value="item"
+                            :checked="thisPrice === item">
+                        <label :for="'price' + index" class="cursor-pointer">{{ item }}</label>
                     </div>
                 </div>
             </div>
@@ -132,17 +156,18 @@ export default {
 
         <hr class="mt-6 mb-12 border-[#aaa9a9]">
 
-        <RouterLink :to="{name:'TackCaseDetailsPage',params:{thisList:'abc'}}" class=" relative" v-for="index in 5">
-            <h1 class=" font-bold text-xl">照片修改後製</h1>
+        <RouterLink :to="{ name: 'TackCaseDetailsPage', params: { thisList: JSON.stringify(item) } }" class=" relative"
+            v-for="(item, index) in arrList">
+            <h1 class=" font-bold text-xl">{{ item.title }}</h1>
             <div class="flex">
-                <p class="text-red-600 font-bold text-lg">1萬 ~ 5萬</p>
+                <p class="text-red-600 font-bold text-lg">{{ item.price }}</p>
                 <div class="flex items-center ml-4">
                     <img src="../assets/img/TackCaseImg/location.png" alt="" width="15" class="h-[15px]">
-                    <p class="text-[#7F7F7F] ml-2 font-bold text-lg">台北</p>
+                    <p class="text-[#7F7F7F] ml-2 font-bold text-lg">{{ item.location }}</p>
                 </div>
-                <div class="flex items-center ml-4" >
+                <div class="flex items-center ml-4">
                     <img src="../assets/img/TackCaseImg/date.png" alt="" width="15" class="h-[15px]">
-                    <p class="text-[#7F7F7F] ml-2 font-bold text-lg">2023/09/30</p>
+                    <p class="text-[#7F7F7F] ml-2 font-bold text-lg">{{ item.caseDate }}</p>
                 </div>
             </div>
 
@@ -152,10 +177,12 @@ export default {
                     text-overflow: ellipsis;
                     white-space: nowrap;
              -->
-            <p class=" text-xl font-bold py-6 pl-6 pr-12 whitespace-nowrap truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora aut soluta dolor nisi recusandae dolorum aperiam, repellat doloremque placeat, sunt dignissimos quasi quos pariatur impedit itaque. Praesentium quod cumque ratione nobis iusto aliquam repudiandae dolor sapiente enim doloremque minima ad officiis placeat aspernatur illum reiciendis modi, itaque quo? Nemo?</p>
-        
+            <p class=" text-xl font-bold py-6 pl-6 pr-12 whitespace-nowrap truncate">{{ item.content }}</p>
+
             <div class=" text-end py-3">
-                <RouterLink to="/" class=" text-white rounded-lg bg-[#FF6E6E] py-3 px-6 mr-12 text-2xl font-bold hover:scale-105 active:scale-95">我要接案</RouterLink>
+                <RouterLink to="/"
+                    class=" text-white rounded-lg bg-[#FF6E6E] py-3 px-6 mr-12 text-2xl font-bold hover:scale-105 active:scale-95">
+                    我要接案</RouterLink>
             </div>
             <hr class="my-6 border-[#cecece]">
         </RouterLink>
@@ -165,110 +192,125 @@ export default {
             <p class="text-xl">你可以嘗試更換你的篩選條件或關鍵字</p>
         </div>
 
-        <div v-if="page!==0" class="flex justify-end items-end">
-           <button v-if="page !== 1" type="button" class="pageBtn" @click="pageBlank()">上一頁</button>
+        <div v-if="page !== 0" class="flex justify-end items-end">
+            <button v-if="page !== 1" type="button" class="pageBtn" @click="pageBlank()">上一頁</button>
 
-           <button v-if="page > Math.floor(this.pageNum/2)+1" type="button" class="pageBtn" @click="page = 1">1</button>
-           <div v-if="page > Math.floor(this.pageNum/2)+2" type="button" class="mx-3 text-lg font-bold">...</div>
+            <button v-if="page > Math.floor(this.pageNum / 2) + 1" type="button" class="pageBtn" @click="page = 1">1</button>
+            <div v-if="page > Math.floor(this.pageNum / 2) + 2" type="button" class="mx-3 text-lg font-bold">...</div>
 
-           <button type="button" :class="{'pageBtn':true,'bg-[#ffc8d1] text-[#E12D4A]':index === page}"  v-for="(index) in pageNumCheck" @click="page = index">{{ index }}</button>
+            <button type="button" :class="{ 'pageBtn': true, 'bg-[#ffc8d1] text-[#E12D4A]': index === page }"
+                v-for="(index) in pageNumCheck" @click="page = index">{{ index }}</button>
 
-           <div v-if="page + Math.floor(this.pageNum/2)+1 < allPage" type="button" class="mx-3 text-[1.125rem] font-bold">...</div>
-           <button v-if="page + Math.floor(this.pageNum/2) < allPage" type="button" class="pageBtn" @click="page = allPage">{{ allPage }}</button>
+            <div v-if="page + Math.floor(this.pageNum / 2) + 1 < allPage" type="button" class="mx-3 text-[1.125rem] font-bold">
+                ...</div>
+            <button v-if="page + Math.floor(this.pageNum / 2) < allPage" type="button" class="pageBtn"
+                @click="page = allPage">{{ allPage }}</button>
 
-           <button v-if="page !== allPage" type="button" class="pageBtn" @click="pageNext()">下一頁</button>
+            <button v-if="page !== allPage" type="button" class="pageBtn" @click="pageNext()">下一頁</button>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+// 遠端 現場
+.findCaseType {
+    text-align: center;
+    color: #7F7F7F;
+    font-size: 1.5rem;
+    line-height: 2rem;
+    font-weight: 700;
+    margin: 0 1.5rem;
 
-    // 遠端 現場
-    .findCaseType{
-        text-align: center;
-        color: #7F7F7F;
-        font-size: 1.5rem;
-        line-height: 2rem;
-        font-weight: 700;
-        margin: 0 1.5rem;     
+    &:hover {
+        scale: 1.05;
+    }
 
-        &:hover{
+    &:hover>iconify-icon,
+    &:hover>iconify-icon+p {
+        color: rgb(220, 38, 38);
+    }
+
+    &:active {
+        scale: 0.95;
+    }
+}
+
+// 案件金額 地區 選擇框
+.filter {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.75rem 1.5rem;
+    margin-right: 0.75rem;
+    border: 2px #454545 solid;
+    border-radius: 0.5rem;
+    width: 175px;
+    color: #454545;
+    font-weight: bold;
+    cursor: pointer;
+}
+
+// 關鍵字
+.searchKeyWords {
+    outline: none;
+    border: 2px #454545 solid;
+    border-radius: 0.5rem 0 0 0.5rem;
+    padding: 0.5rem 1rem;
+    color: #454545;
+    font-weight: bolder;
+    cursor: pointer;
+
+    &::placeholder {
+        color: #454545;
+    }
+
+    &+img {
+        background-color: #7F7F7F;
+        padding: 0.75rem;
+        border-radius: 0 0.5rem 0.5rem 0;
+        cursor: pointer;
+
+        &:hover {
             scale: 1.05;
         }
-        &:active{
+
+        &:active {
             scale: 0.95;
         }
     }
+}
 
-    // 案件金額 地區 選擇框
-    .filter{
-        display: flex; justify-content: space-between;  align-items: center;
-        padding: 0.75rem 1.5rem;
-        margin-right: 0.75rem;
-        border: 2px #454545 solid; border-radius: 0.5rem;
-        width: 175px;
-        color: #454545;
-        font-weight: bold;
-        cursor: pointer;
+// 下拉選單
+.mapAndPriceSelect {
+    // padding: 0.75rem 2rem 0.75rem 1.5rem;
+    position: absolute;
+    background: white;
+    max-height: 200px;
+    overflow: auto;
+    border: 2px rgb(200, 200, 200) solid;
+    border-radius: 0.75rem;
+    z-index: 50;
+
+    &::-webkit-scrollbar-thumb {
+        background: #9b9b9b;
+        border-radius: 9999px;
     }
 
-    // 關鍵字
-    .searchKeyWords{
-        outline: none;
-        border: 2px #454545 solid; border-radius: 0.5rem 0 0 0.5rem;
-        padding: 0.5rem 1rem;
-        color: #454545;
-        font-weight: bolder;
-        cursor: pointer;
-
-        &::placeholder{
-            color: #454545;
-        }
-
-        &+img{
-            background-color: #7F7F7F;
-            padding: 0.75rem;
-            border-radius: 0 0.5rem 0.5rem 0;
-            cursor: pointer;
-
-            &:hover{
-                scale: 1.05;
-            }
-            &:active{
-                scale: 0.95;
-            }
-        }
+    &::-webkit-scrollbar {
+        width: 10px;
     }
+}
 
-    // 下拉選單
-    .mapAndPriceSelect{
-        // padding: 0.75rem 2rem 0.75rem 1.5rem;
-        position: absolute;
-        background: white;
-        max-height: 200px;
-        overflow: auto;
-        border: 2px rgb(200, 200, 200) solid; border-radius: 0.75rem;
-        z-index: 50;
+// 分頁
+.pageBtn {
+    border: gray 2px solid;
+    padding: 0.75rem;
+    margin: 0 0.25rem;
+    font-size: 1.125rem;
+    line-height: 1.125rem;
+    font-weight: bold;
 
-        &::-webkit-scrollbar-thumb{
-            background: #9b9b9b;
-            border-radius: 9999px;
-        }
-        &::-webkit-scrollbar{
-            width: 10px;
-        }
+    &:hover {
+        background: #ffc8d1;
     }
-
-    // 分頁
-    .pageBtn{
-        border: gray 2px solid;
-        padding: 0.75rem; margin: 0 0.25rem;
-        font-size: 1.125rem;
-        line-height: 1.125rem;
-        font-weight: bold;
-
-        &:hover{
-            background: #ffc8d1;
-        }
-    }
-</style>
+}</style>
