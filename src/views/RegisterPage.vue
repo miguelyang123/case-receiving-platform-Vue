8423b28c82ac4d97e9fd421e680210b43252f19a
syaun <clou40308@gmail.com>
2023-09-16 15:42:31 +0800

    UPDATE:新增axios 以及註冊頁面API串接已完成

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,24 +1,26 @@
 <script>
 import router from '../router';
+import axios from 'axios';
 export default {
   data() {
     return {
       pwdFlag: false, //密碼明碼
       repeatpwdFlag: false, //repeat 密碼明碼
+      responseData: null,
+      repeatPassword: "",
+      code: "",
+      message: "",
       //json
-      formData: {
-        name: "",//姓名
-        phone: "",//電話
+      postData: {
         email: "",//email
         password: "",//密碼
-        repeatPassword: "",//確認密碼
+        user_name: "",//姓名
+        phone: "",//電話  
       }
     }
   },
-  computed:{
-    formDataJSON() {
-      return JSON.stringify(this.formData);
-    }
+  computed: {
+
   },
   methods: {
     pwdflagTrue() {
@@ -37,7 +39,45 @@ export default {
       router.push("/login_page")
     },
     register() {
-      console.log(this.formDataJSON);
+      const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      const pwdPattern = /^.{8,30}$/;
+      const namePattern = /^.{2,30}$/;
+      const phonePattern = /^(?:0|\d{3}-?)(9|2)\d{2}-?\d{6}$/;
+      if (!emailPattern.test(this.postData.email)) {
+        alert("請輸入有效的email!");
+        return;
+      }
+      if (!pwdPattern.test(this.postData.password)) {
+        alert("請輸入有效的密碼(長度須介於8個字元跟30字元之間)!");
+        return;
+      }
+      if (!namePattern.test(this.postData.name)) {
+        alert("請輸入有效的名字!");
+        return;
+      }
+      if (!phonePattern.test(this.postData.phone)) {
+        alert("請輸入有效的手機門號!");
+        return;
+      }
+      if (this.postData.password !== this.repeatPassword) {
+        alert("輸入密碼與確認密碼不一致!");
+        return;
+      }
+      axios.post('http://localhost:8080/api/signup', this.postData)
+        .then(response => {
+          this.responseData = response;
+          this.code = this.responseData.data.code;
+          this.message = this.responseData.data.message;
+          if (this.code === "200") {
+            alert(this.message);
+            router.push("/login_page");
+          } else {
+            alert(this.message);
+          }
+        })
+        .catch(error => {
+          alert(error);
+        });
     }
   },
 }
@@ -57,14 +97,14 @@ export default {
       <div class="flex">
         <div class="w-6/12  h-[350px]">
           <div class="relative">
-            <input type="text" placeholder="輸入您的真實姓名" v-model="formData.name"
+            <input type="text" placeholder="輸入您的真實姓名" v-model="postData.user_name"
               class="border-2 border-black ml-[50px] mt-[80px] w-[400px] h-[55px] text-[24px] rounded-lg pl-[60px]">
             <div
               class="absolute w-[35px] h-[35px] bg-[url('../../public/icon-park-twotone_edit-name.png')] bg-cover bottom-[9px] left-[60px]">
             </div>
           </div>
           <div class="relative">
-            <input type="text" placeholder="輸入您的手機門號" v-model="formData.phone"
+            <input type="text" placeholder="輸入您的手機門號" v-model="postData.phone"
               class="border-2 border-black ml-[50px] mt-[50px] w-[400px] h-[55px] text-[24px] rounded-lg pl-[60px]">
             <div
               class="absolute w-[35px] h-[35px] bg-[url('../../public/ph_phone-duotone.png')] bg-cover bottom-[9px] left-[60px]">
@@ -73,14 +113,14 @@ export default {
         </div>
         <div class="w-6/12  h-[350px]">
           <div class="relative">
-            <input type="text" placeholder="輸入Email" v-model="formData.email"
+            <input type="text" placeholder="輸入Email" v-model="postData.email"
               class="border-2 border-black ml-[50px] mt-[40px] w-[400px] h-[55px] text-[24px] rounded-lg pl-[60px]">
             <div
               class="absolute w-[35px] h-[35px] bg-[url('../../public/Account_icon.png')] bg-cover bottom-[9px] left-[60px]">
             </div>
           </div>
           <div class="relative">
-            <input placeholder="輸入密碼" :type="pwdFlag ? 'text' : 'password'" v-model="formData.password"
+            <input placeholder="輸入密碼(長度8字元以上)" :type="pwdFlag ? 'text' : 'password'" v-model="postData.password"
               class="border-2 border-black ml-[50px] mt-[50px] w-[400px] h-[55px] text-[24px] rounded-lg px-[60px]">
             <div class="absolute w-[35px] h-[35px] bg-[url('../../Password_icon.png')] bg-cover bottom-[9px] left-[60px]">
             </div>
@@ -90,7 +130,7 @@ export default {
               v-if="this.pwdFlag === true"></i>
           </div>
           <div class="relative ">
-            <input placeholder="確認密碼" :type="repeatpwdFlag ? 'text' : 'password'" v-model="formData.repeatPassword"
+            <input placeholder="確認密碼" :type="repeatpwdFlag ? 'text' : 'password'" v-model="repeatPassword"
               class="border-2 border-black ml-[50px] mt-[50px] w-[400px] h-[55px] text-[24px] rounded-lg px-[60px] ">
             <div class="absolute w-[35px] h-[35px] bg-[url('../../Password_icon.png')] bg-cover bottom-[9px] left-[60px]">
             </div>
