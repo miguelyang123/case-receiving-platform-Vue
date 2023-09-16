4cfa0276688b9f73ff4529b1dc1e031f63a4649c
syaun <clou40308@gmail.com>
2023-09-16 16:35:47 +0800

    UPDATE:登入API以串接完成

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,11 +1,17 @@
 <script >
 import router from '../router';
+import axios from 'axios';
 export default {
     data() {
         return {
             pwdFlag: false, //密碼明碼
-            email:"",//email
-            password: "",//密碼
+            code: "",
+            message: "",
+            //json
+            postData: {
+                email: "",//email
+                password: "",//密碼
+            }
         }
     },
     methods: {
@@ -15,9 +21,26 @@ export default {
         pwdflagFalse() {
             this.pwdFlag = false;
         },
-        registerRouterPush(){
-           router.push("/register_page")
+        registerRouterPush() {
+            router.push("/register_page")
         },
+        login() {
+            axios.post('http://localhost:8080/api/login', this.postData)
+                .then(response => {
+                    this.responseData = response;
+                    this.code = this.responseData.data.code;
+                    this.message = this.responseData.data.message;
+                    if (this.code === "200") {
+                        alert(this.message);
+                        router.push("/personal_info");
+                    } else {
+                        alert(this.message);
+                    }
+                })
+                .catch(error => {
+                    alert(error);
+                });
+        }
     },
 }
 </script>
@@ -35,14 +58,14 @@ export default {
             </div>
             <div class="h-[220px] ">
                 <div class="relative">
-                    <input type="text" placeholder="輸入Email" v-model="email"
+                    <input type="text" placeholder="輸入Email" v-model="postData.email"
                         class="border-2 border-black ml-[50px] mt-[40px] w-[400px] h-[55px] text-[24px] rounded-lg pl-[60px]">
                     <div
                         class="absolute w-[35px] h-[35px] bg-[url('../../public/Account_icon.png')] bg-cover bottom-[9px] left-[60px]">
                     </div>
                 </div>
                 <div class="relative">
-                    <input placeholder="輸入密碼" :type="pwdFlag ? 'text' : 'password'" v-model="password"
+                    <input placeholder="輸入密碼" :type="pwdFlag ? 'text' : 'password'" v-model="postData.password"
                         class="border-2 border-black ml-[50px] mt-[50px] w-[400px] h-[55px] text-[24px] rounded-lg px-[60px]">
                     <div
                         class="absolute w-[35px] h-[35px] bg-[url('../../Password_icon.png')] bg-cover bottom-[9px] left-[60px]">
@@ -54,12 +77,14 @@ export default {
                 </div>
             </div>
             <div class="h-[50px]  flex  justify-between px-[60px]">
-                <button type="button" class="text-[22px] font-bold hover:scale-105 active:scale-95 " @click="registerRouterPush">會員註冊</button>
+                <button type="button" class="text-[22px] font-bold hover:scale-105 active:scale-95 "
+                    @click="registerRouterPush">會員註冊</button>
                 <button type="button" class="text-[22px] font-bold hover:scale-105 active:scale-95 ">忘記密碼</button>
             </div>
             <div class="flex  h-[120px] justify-center items-center ">
                 <button type="button"
-                    class="w-[220px] h-[60px]  text-[26px] rounded-lg bg-[#2B4BF0] text-white hover:scale-105 active:scale-95">登入</button>
+                    class="w-[220px] h-[60px]  text-[26px] rounded-lg bg-[#2B4BF0] text-white hover:scale-105 active:scale-95"
+                    @click="login">登入</button>
             </div>
         </div>
     </div>
