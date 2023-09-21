8f93a995e71319dce6d3f02ccade2e16e4752539
hibiki4945 <wc4910720@gmail.com>
2023-09-21 09:33:24 +0800

    done

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+
+import Vue from 'vue'
 // import uploader from 'vue-simple-uploader'
 // Vue.use(uploader)
 
